fc57c449b57356d429c97f3eef213f16f139a0d7
Max Yuzva <maxyuzva@MacBook-Pro-Max.local>
2018-04-01 13:48:16 +0300

    leaflet hell added

--- src/App.js
+++ src/App.js
@@ -1,21 +1,38 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Map } from 'react-leaflet';
+const { LeafletMap, TileLayer, Marker, Popup } = Map;
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    
+    constructor() {
+    super()
+        this.state = {
+            lat: 51.505,
+            lng: -0.09,
+            zoom: 13
+        }
+    }
+
+    render() {
+        const position = [this.state.lat, this.state.lng];
+        return (
+            <div className="App">
+                <LeafletMap center={position} zoom={this.state.zoom}>
+                    {/* <TileLayer
+                        attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                        url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+                    /> */}
+                    <Marker position={position}>
+                        <Popup>
+                            <span>A pretty CSS3 popup. <br /> Easily customizable.</span>
+                        </Popup>
+                    </Marker>
+                </LeafletMap>
+            </div>
+        );
+    }
 }
 
 export default App;
